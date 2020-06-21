fd8abc0b3adfea9c66c5d2f26d9123e064909d70
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-21 13:49:22 -0600

    Initial Project Commit
    Brick breaker type game

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidth = 16f;
+    [SerializeField] float mouseMin = 1f;
+    [SerializeField] float mouseMax = 15f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidth;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePos, mouseMin, mouseMax);
+        transform.position = paddlePos;
+    }
+}
