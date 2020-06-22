19ff475dce7acf81bd2866a4aff7655afbdb2604
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-22 15:08:31 -0600

    Added CountBreakableBlocks
    Counts the number of breakbale blocks within scene, Serialized for debugging.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+     
+     //cached reference
+     Level level;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 1f);
         Destroy(gameObject, 0f);
+    }
+   
+   
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
     }
 }
