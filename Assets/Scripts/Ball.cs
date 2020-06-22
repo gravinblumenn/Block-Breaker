ad38765698365553d90140357963dc6a5786cc54
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-22 10:10:33 -0600

    Launch Ball on Mouse click
    created variable hasStarted to controll the lockBallToPaddle based on bool value initialized at the beginning and changed once clicked.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
     Vector2 paddleToBallVector;
+    bool hasStarted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,23 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
+    }
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2 (xPush, yPush);
+        }
     }
 }
