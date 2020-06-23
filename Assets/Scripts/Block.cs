a7b2475f5c2a822cd133e953b3074a6a9b408bac
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-23 10:46:15 -0600

    DestroyBlocks brings the game to the next level(Scene)
    added method to brings game to next scene once all blocks on current scene have been destroyed. reformated the OnCollisionEnter2D method

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -10,11 +10,16 @@ public class Block : MonoBehaviour
      Level level;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 1f);
         Destroy(gameObject, 0f);
+        level.BlockDestroyed();
     }
-   
-   
+
     private void Start()
     {
         level = FindObjectOfType<Level>();
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,9 +6,24 @@ public class Level : MonoBehaviour
 {
     [SerializeField] int breakableBlocks; //serialized for debugging purposes
     
+    //Cached Reference
+    SceneLoader sceneLoader;
+    private void Start() 
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();   
+    }
     
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
