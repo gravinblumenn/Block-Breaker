bf7d3886abf1464c0f9e8f63e8f996a6098a16b6
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-23 15:38:33 -0600

    Instantiation of VFX and tags
    added partical effect when destroying blocks through instantiation. created 2 tags, breakable and unbreakable and adjusted code to only count breakable blocks and still allow a win with "unbreakable" tagged blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,27 +5,52 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
      
      //cached reference
      Level level;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (tag == "Breakable")
+        {
         DestroyBlock();
+        }
     }
 
     private void DestroyBlock()
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 1f);
+        PlayBlockSFX();
         Destroy(gameObject, 0f);
         level.BlockDestroyed();
-        
+
+        TriggerSparklesVFX();
+    }
+
+    private void PlayBlockSFX()
+    {
         //adds score per block destroyed
         FindObjectOfType<GameStatus>().AddToScore();
+
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 1f);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 2f);
     }
 
     private void Start()
     {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
         level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
     }
 }
