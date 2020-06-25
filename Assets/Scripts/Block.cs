11877753be2a9dd165502d8a438c0ef65eb6db63
Gavin Blumenshein <gavinblumenn@gmail.com>
2020-06-25 15:15:31 -0600

    Added error message for array, smarter masxHits logic
    array error message will tell us which gameObject is causing the issue. maxHits will now =  length of sprite index + 1

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,36 +6,45 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits;
+
     [SerializeField] Sprite[] hitSprites;
-     
+
      //cached reference
      Level level;
 
      //state variable
      [SerializeField] int timesHit; //serialized for debugging purposes
-    private void OnCollisionEnter2D(Collision2D collision)
+     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (tag == "Breakable")
         {
             HandleHit();
         }
-        
+
     }
     private void ShowNextHitSprite()
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[sprideIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array " + gameObject.name);
+        }
+
     }
 
     private void HandleHit()
     {
          timesHit++;
+         int maxHits = hitSprites.Length + 1;
          if (timesHit >= maxHits)
         {
              DestroyBlock();
         }
-        else 
+        else
         {
             ShowNextHitSprite();
         }
